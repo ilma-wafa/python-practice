66580714a1c498007d191350d8ed06db1dc3243b
ilmawf <ilmafwafa@gmail.com>
2026-04-30 19:29:09 +0530

    Added return value

--- functions.py
+++ functions.py
@@ -2,4 +2,5 @@ def greeting(name, department):
     print("Welcome, " + name)
     print("You are part of " + department)
 
-greeting("Welma", "Finance")
+greeting("Welma", "Finance team")
+greeting("Amy", "Development team")
