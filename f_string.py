60d7da9b2d2ef11e4de4f753aa9b288cf9c07aee
ilmawf <ilmafwafa@gmail.com>
2026-05-13 16:07:48 +0530

    Praticed F-String

---
+++ f_string.py
@@ -0,0 +1,2 @@
+for number in  range(1, 11, 1):
+    print(f"5 x {number} = {5 * number}")
--- range.py
+++ range.py
@@ -1,2 +1,4 @@
-for n in range(x, y, z):
-    print(n)
+total = 0
+for number in range(0 , 101, 1):
+    total = total + number
+print(total)
