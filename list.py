d3b06cf22e2c95826cda0b9f258b1a588b43875d
ilmawf <ilmafwafa@gmail.com>
2026-05-15 15:51:51 +0530

    Worked on List

---
+++ list.py
@@ -0,0 +1,17 @@
+x = ["Now", "we", "are", "cooking!"]
+type(x)
+print (type(x))
+
+print (len(x))
+
+print ("are" in x)
+
+print ("Today" in x)
+
+print (x[0])
+
+print (x[3])
+
+print (x[1:3])
+
+print (x[:2])
