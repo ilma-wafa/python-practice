bea156ab0cb79ff5206d54bc3372173bf415165f
ilmawf <ilmafwafa@gmail.com>
2026-05-18 20:34:26 +0530

    Add name box practice

---
+++ print.py
@@ -0,0 +1,7 @@
+def name_box(name):
+    len() = name
+    print(len ("*********************"))
+    print(f"*   {name}   *")     
+    print(len ("*********************"))
+
+name_box("Ilma Wafa from Sri Lanka") 
